5bbcbafa2dedb5bfb44c5ea75660a2e7ab8d91b1
Fernando Perez <fernando.perez.lara@outlook.com>
2025-05-08 11:51:26 +0200

    chore: add frontend interface

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "@/styles/reset.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
